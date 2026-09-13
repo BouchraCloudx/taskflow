bfc4c4e76754c362fe18da8317d7e20cec640e75
BouchraCloudx <bushrael90@gmail.com>
2026-09-13 14:05:31 +0100

    Configure S3 remote state for Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.9"
 
+    backend "s3" {
+    bucket         = "taskflow-tf-state-bush"
+    key            = "taskflow/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "taskflow-tf-lock"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
